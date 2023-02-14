a55c9c19aca9189b907ab49020765cf77abddbaf
psalmistcom <dadewale9@gmail.com>
2023-02-14 08:12:23 +0100

    review done

--- v2/dav/reviews.php
+++ v2/dav/reviews.php
@@ -41,6 +41,11 @@
                     <div class="col-md-10 offset-md-1 col-lg-8 offset-lg-2 mx-auto text-center">
                         <h2 class="fs-15 text-uppercase text-muted mb-3">Happy Customers</h2>
                         <h3 class="display-4 mb-10 px-xl-10 px-xxl-15">CLIENT'S REVIEW AND REPORT.</h3>
+                        <h6>Hello dear friend, I officially welcome you here. 
+                           I want you to know that over 500 people have consulted my services (Within Nigeria, Abroad, online and offline) 
+                           with several testimonies and they are presently living the best version of themselves. 
+                           The clinic also has an E-library for clients' rapid healing during and after therapy.
+                            There is also a provision for consistent follow-ups to avoid a relapse.</h6>
                     </div>
                     <!-- /column -->
                 </div>
@@ -52,26 +57,22 @@
                             <div class="card">
                                 <div class="card-body">
                                     <blockquote class="icon mb-0">
-                                        <p>“Why can you say about the service of therapist
-                                            Abigail?.......,......,...............Response: I was having a difficult
-                                            time taking some decisions. It was really weighing me down so I needed to
+                                        <p>“I was having a difficult time taking some decisions.
+                                            It was really weighing me down so I needed to
                                             talk to a professional about it. I had been searching for one for a long
                                             time but couldn't find someone I had no doubt about until I was referred to
                                             therapist Abigail. I enjoyed speaking with her. She's calm and listens
                                             actively. Her advice are also very helpful if followed. She helped me see
                                             the challenge I had in a different light and after our discussion, I feel
                                             much better than I was. Before consulting therapist Abigail my mental health
-                                            status was 3, now my mental health status is 6. I booked for one session.
-                                            ,.............,................................... My advice to those who
-                                            will read my feedback is they should not hold back, have no fear because
-                                            therapist Abigail is helping me and can help you through every psychological
-                                            disorder just as she promises. I strongly recommend therapist Abigail.
-
-                                            ”</p>
+                                            status was 3, now my mental health status is 6. I booked for one session. 
+                                            My advice to those who will read my feedback is they should not hold back,
+                                            have no fear because therapist Abigail is helping me and can help you through every psychological
+                                            disorder just as she promises. I strongly recommend therapist Abigail.”</p>
                                         <div class="blockquote-details">
                                             <div class="">
                                                 <h5 class="mb-1">CE - </h5>
-                                                <p class="mb-0 text-warning">Booked for 1 online therapy session from
+                                                <p class="mb-0 text-warning">Booked for 1-month online therapy session from
                                                     Lagos</p>
                                             </div>
                                         </div>
@@ -81,31 +82,69 @@
                             </div>
                             <!-- /.card -->
                         </div>
-
                         <div class="item col-md-6 col-xl-6">
                             <div class="card">
                                 <div class="card-body">
                                     <blockquote class="icon mb-0">
-                                        <p>“Why can you say about the service of therapist
-                                            Abigail?.......,......,...............Response: I was having a difficult
-                                            time taking some decisions. It was really weighing me down so I needed to
-                                            talk to a professional about it. I had been searching for one for a long
-                                            time but couldn't find someone I had no doubt about until I was referred to
-                                            therapist Abigail. I enjoyed speaking with her. She's calm and listens
-                                            actively. Her advice are also very helpful if followed. She helped me see
-                                            the challenge I had in a different light and after our discussion, I feel
-                                            much better than I was. Before consulting therapist Abigail my mental health
-                                            status was 3, now my mental health status is 6. I booked for one session.
-                                            ,.............,................................... My advice to those who
-                                            will read my feedback is they should not hold back, have no fear because
-                                            therapist Abigail is helping me and can help you through every psychological
-                                            disorder just as she promises. I strongly recommend therapist Abigail.
-
-                                            ”</p>
+                                        <p>“I was depressed (I already wrote out ten different ways to die without pain.
+                                            I was ready to leave my family and let them enjoy freedom from my mental health challenges but what stopped me from committing suicide was my religious belief).
+                                            I was referred to 3 different psychologists and doctors but they were very unprofessional and tactless.
+                                            A friend referred therapist Abigail to my wife and not wanting to appear like someone who does not want to heal I decided to give therapist Abigail a chance and I am glad I did so.
+                                            My mental health status before consulting her services was 0, now my mental health status is 9.
+                                            She is very professional, intelligent and certain. My advice to those who will read my review is your mental health is very important. When needed, get help at all cost. 
+                                            You will be glad you did. I Strongly recommend Therapist Abigail.</p>
                                         <div class="blockquote-details">
                                             <div class="">
-                                                <h5 class="mb-1">CE - </h5>
-                                                <p class="mb-0 text-warning">Booked for 1 online therapy session from
+                                                <h5 class="mb-1">AA- </h5>
+                                                <p class="mb-0 text-primary">Booked for 2-months online therapy session from
+                                                    Ekiti</p>
+                                            </div>
+                                        </div>
+                                    </blockquote>
+                                </div>
+                                <!-- /.card-body -->
+                            </div>
+                            <!-- /.card -->
+                        </div>
+                        <div class="item col-md-6 col-xl-6">
+                            <div class="card">
+                                <div class="card-body">
+                                    <blockquote class="icon mb-0">
+                                        <p>“I was depressed. My mental health status before consulting therapist Abigail was 2, now it is 7. 
+                                            Before we stumbled on one of therapist Abigail's post on personality disorders, 
+                                            my family and I already sought for therapists but they were very unprofessional and do not know what to do.
+                                            Therapist Abigail service was exactly what we needed. She knew what to do and say per time. She is very helpful.
+                                             My advice to those who will read my feedback is that follow every instruction given by the therapist.
+                                            There is light at the end of the tunnel.</p>
+                                        <div class="blockquote-details">
+                                            <div class="">
+                                                <h5 class="mb-1">AT- </h5>
+                                                <p class="mb-0 text-info">Booked for 1-month online therapy session from
+                                                    Adamawa</p>
+                                            </div>
+                                        </div>
+                                    </blockquote>
+                                </div>
+                                <!-- /.card-body -->
+                            </div>
+                            <!-- /.card -->
+                        </div>
+                        <div class="item col-md-6 col-xl-6">
+                            <div class="card">
+                                <div class="card-body">
+                                    <blockquote class="icon mb-0">
+                                        <p>“ I was traumatized from several episodes of marital violence. My religious leaders and people around were not supportive, 
+                                            they kept telling me to go back to my husband, be a submissive wife and be more prayerful. 
+                                            The last violence broke that broke the back of the carmel was when he hit me with an object that dislocated my right hand and landed me in an orthopaedic hospital.
+                                            He never paid the bills, he left me there and went to church for an ongoing program.
+                                            My mental health status before consulting therapist Abigail was 1, now my mental health status is 8.
+                                            My advice to those who will read my feedback is that they should take their mental health seriously.
+                                            Therapist Abigail is very friendly, a good listener and highly competent. 
+                                            I strongly recommend therapist Abigail.</p>
+                                        <div class="blockquote-details">
+                                            <div class="">
+                                                <h5 class="mb-1">AK- </h5>
+                                                <p class="mb-0 text-danger">Booked for 1-months online therapy session from
                                                     Lagos</p>
                                             </div>
                                         </div>
@@ -115,11 +154,81 @@
                             </div>
                             <!-- /.card -->
                         </div>
-
+                        <div class="item col-md-6 col-xl-6">
+                            <div class="card">
+                                <div class="card-body">
+                                    <blockquote class="icon mb-0">
+                                        <p>“ I was traumatized and having PTSD from several episodes of childhood and marital violence.
+                                            My mental health status before consulting therapist Abigail was 3, now my mental health status is 8.
+                                            My advice to those who will read my feedback is that they should take their mental health seriously.
+                                            Therapist Abigail is very friendly, patient, a good listener, never judge, very supportive and highly competent.
+                                            I strongly recommend therapist Abigail.</p>
+                                        <div class="blockquote-details">
+                                            <div class="">
+                                                <h5 class="mb-1">OO- </h5>
+                                                <p class="mb-0 text-purple">Booked for 1-month online therapy session from
+                                                    Owerri</p>
+                                            </div>
+                                        </div>
+                                    </blockquote>
+                                </div>
+                                <!-- /.card-body -->
+                            </div>
+                            <!-- /.card -->
+                        </div>
+                        <div class="item col-md-6 col-xl-6">
+                            <div class="card">
+                                <div class="card-body">
+                                    <blockquote class="icon mb-0">
+                                        <p>“I was having major depression disorder with suicidal thoughts and a few attempted suicide.
+                                            My mental health status before consulting therapist Abigail was 2, now my mental health status is 8.
+                                            My advice to those who will read my feedback is that they should take they should not let the negative things that people say about seeking for a mental health therapist to discourage them.
+                                            Nigeria is still backward in mental health awareness and I am glad that therapist Abigail is giving her best to close the gap.
+                                            Therapist Abigail doubles as an amazing therapist and an excellent friend, very supportive and highly competent.
+                                            I strongly recommend therapist Abigail.</p>
+                                        <div class="blockquote-details">
+                                            <div class="">
+                                                <h5 class="mb-1">BA- </h5>
+                                                <p class="mb-0 text-green">Booked for 1-month online therapy session from
+                                                    Abuja</p>
+                                            </div>
+                                        </div>
+                                    </blockquote>
+                                </div>
+                                <!-- /.card-body -->
+                            </div>
+                            <!-- /.card -->
+                        </div>
+                        <div class="item col-md-6 col-xl-6">
+                            <div class="card">
+                                <div class="card-body">
+                                    <blockquote class="icon mb-0">
+                                        <p>“My son was a handful and his mental health status was really affecting mine.
+                                            He inherited a lot of psychotic and psychological disorder from my family.
+                                            I stumbled on therapist Abigail's post about DID (Dissociative identity disorder) on facebook,
+                                            I engaged the comment box then I sent her a DM immediately. 
+                                            My advice to those who will read my feedback is that with therapist Abigail, distance is nothing.
+                                            She is an expert in connecting your mind to your body. You can not be disconnected when having a session with her.
+                                            If her services are this exceptional over a distance like the USA to Nigeria,
+                                            I can't imagine the wonders that will happen when it is an offline session.
+                                            I strongly recommend therapist Abigail.</p>
+                                        <div class="blockquote-details">
+                                            <div class="">
+                                                <h5 class="mb-1">CO- </h5>
+                                                <p class="mb-0 text-blue">Booked for two weeks online therapy session from
+                                                    USA</p>
+                                            </div>
+                                        </div>
+                                    </blockquote>
+                                </div>
+                                <!-- /.card-body -->
+                            </div>
+                            <!-- /.card -->
+                        </div>
 
                     </div>
                     <!-- /.row -->
-                </div>
+                
                 <!-- /.grid-view -->
             </div>
             <!-- /.container -->
